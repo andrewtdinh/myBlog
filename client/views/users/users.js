f2dcba5c67dfe5e4501bce1fef1edc882afb516e
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-25 14:46:14 -0700

    injecting rootScope, User, and $state into Users Controller

--- client/views/users/users.js
+++ client/views/users/users.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('ptc')
-.controller('UsersCtrl', ['$scope', function($scope){
-  // body...
+.controller('UsersCtrl', ['$scope', '$rootScope', '$state', 'User', function($scope, $rootScope, $state, User){
+  $scope.submit = function(user){
+    console.info('$state is:', $state.current.name);
+  };
 }]);

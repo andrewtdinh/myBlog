f66acc546c1aa2b907d386b7b4b751d782c555a2
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-25 16:22:57 -0700

    Negative test user registration and route user to login page after successful user registration

--- client/views/users/users.js
+++ client/views/users/users.js
@@ -10,6 +10,7 @@ angular.module('ptc')
       User.register(user)
       .then(function(data){
         console.log('Successfully registerd user with data: ', data);
+        $state.go('login');
       })
       .catch(function(error){
         alert('An error occurred during registration.  Please check your username and password');

5710da7c9361e0b8cba42abb1748030d8fc27ec0
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-25 23:32:23 -0700

    changed font size of items on nav bar and add new blog item

--- client/index.js
+++ client/index.js
@@ -7,7 +7,9 @@ angular.module('ptc', ['firebase', 'ui.router'])
   .state('home', {url: '/', templateUrl: '/views/home/home.html'})
   .state('about', {url: '/about', templateUrl: '/views/home/aboutMe.html'})
   .state('register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
-  .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'});
+  .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
+  .state('addBlog', {url: '/blogs/add', templateUrl: '/views/blogs/blogs.html', controller: 'BlogsCtrl'})
+  .state('editBlog', {url: '/blogs/edit', templateUrl: '/views/blogs/blogs.html', controller: 'BlogsCtrl'});
 }])
 .run(['$rootScope', '$window', '$firebaseAuth', 'firebaseUrl', function($rootScope, $window, $firebaseAuth, firebaseUrl){
   $rootScope.fbRoot = new $window.Firebase(firebaseUrl);

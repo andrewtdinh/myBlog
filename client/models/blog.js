cec3d20e3ddd98f12407b343892bf28ce6c688e5
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-26 13:14:56 -0700

    expand the addBlog function

---
+++ client/models/blog.js
@@ -0,0 +1,6 @@
+'use strict'
+
+angular.module('ptc')
+.factory('Blog', ['$rootScope', function($rootScope){
+  
+}]);
--- client/views/blogs/blogs.js
+++ client/views/blogs/blogs.js
@@ -1,26 +1,37 @@
 'use strict';
 
 angular.module('ptc')
-.controller('BlogsCtrl', ['$scope', '$state', '$rootScope', function($scope, $state, $rootScope){
+.controller('BlogsCtrl', ['Blog', '$window', '$scope', '$state', '$rootScope', 'User', function(Blog, $window, $scope, $state, $rootScope, User){
   $scope.name = $state.current.name;
-  // console.info('$state.current.name: ', $state.current.name);
+  User.init();
+
   console.info('$rootScope.activeUser: ', $rootScope.activeUser);
 
   $scope.addBlog = function(blog){
+    if (!$scope.blog.title){
+      $scope.blog.title = $rootScope.getEmailHandle($state.current.name);
+    }
+    var blurb = makeBlurb($scope.blog.body, 100);
     var o = {
-      author: blog.author,
-      title: blog.title,
-      tags: blog.tags,
-      body: blog.body,
-      createdAt: $window.Firebase.ServerValue.TIMESTAMP
+      author: $scope.blog.author,
+      title: $scope.blog.title,
+      tags: $scope.blog.tags,
+      body: $scope.blog.body,
+      email: $rootScope.activeUser,
+      blurb: blurb,
+      postDate: $window.Firebase.ServerValue.TIMESTAMP
     };
 
-    Task.add(o)
+    Blog.add(o)
     .then(function(data){
       console.info('data', data);
       $scope.blog = {};
     });
   };
-  
+
+  function makeBlurb(post, strLength){
+    return post ? post.slice(0, strLength) : '';
+  }
+
 
 }]);

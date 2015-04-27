58a18a3a8968f626b78e01bf5804d4d2ffeb82c5
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-27 09:53:29 -0700

    toggle between blog body and blurb

--- client/index.js
+++ client/index.js
@@ -6,10 +6,10 @@ angular.module('ptc', ['firebase', 'ui.router'])
   $stateProvider
   .state('home', {url: '/', templateUrl: '/views/home/home.html', controller: 'HomeCtrl'})
   .state('about', {url: '/about', templateUrl: '/views/home/aboutMe.html'})
-  .state('register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
   .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
   .state('addBlog', {url: '/blogs/add', templateUrl: '/views/blogs/blogs.html', controller: 'BlogsCtrl'})
-  .state('editBlog', {url: '/blogs/edit', templateUrl: '/views/blogs/blogs.html', controller: 'BlogsCtrl'});
+  .state('editBlog', {url: '/blogs/edit', templateUrl: '/views/blogs/blogs.html', controller: 'BlogsCtrl'})
+  .state('fullBlog', {url: '/users/{uid}/blogs/{blogKey}/{blogIndex}', templateUrl: '/views/blog/fullBlog.html', controller: 'FullBlogCtrl'});
 }])
 .run(['$rootScope', '$window', '$firebaseAuth', 'firebaseUrl', function($rootScope, $window, $firebaseAuth, firebaseUrl){
   $rootScope.fbRoot = new $window.Firebase(firebaseUrl);
--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('ptc')
-.controller('HomeCtrl', ['$scope', 'User', 'Blog', function($scope, User, Blog){
+.controller('HomeCtrl', ['$state', '$scope', 'User', 'Blog', function($state, $scope, User, Blog){
   User.init();
   $scope.afBlogs = Blog.init();
   $scope.afUsers = Blog.initUsers();
+  // $scope.display = true;
   console.info('Inside the HomeCtrl');
-  console.info('blog.$id: ');
+  $scope.displayFullBlog = function(user, blog){
+    // var userUid = user.$id;
+    // var blogKey = user.
+    // $scope.display = !$scope.display;
+    // $state.go('fullBlog', {uid:,blogKey:,blogIndex: };
+    console.info('user: ', user, 'blog: ', blog);
+  };
 }]);

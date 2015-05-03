2d64753e1c19aa52038536f273c97d991b956b40
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-03 16:41:07 -0700

    Show Edit and Show buttons for login user with uid matching the uid in the blog

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -5,12 +5,16 @@ angular.module('ptc')
   User.init();
   $rootScope.afBlogs = Blog.init();
   $scope.blogs = $rootScope.afBlogs;
+  if($rootScope.activeUser){
+    var userUid = $rootScope.activeUser.uid;
+    $scope.userUid = userUid;   //Handy to hide or show edit and delete buttons.
+  } else {
+    $scope.userUid = '';
+  }
+  console.info('$scope.userUid: ', $scope.userUid);
+  console.info('$rootScope.activeUser: ', $rootScope.activeUser);
 
   $scope.displayFullBlog = function(blog, index){
-    if($rootScope.activeUser){
-      var userUid = $rootScope.activeUser.$id;
-      $scope.userUid = userUid;   //Handy to hide or show edit and delete buttons.
-    }
     var blogKey = $rootScope.afBlogs.$keyAt(index);
     $state.go('fullBlog', {author: blog.author, blogKey: blogKey});
     console.info('blog: ', $rootScope.afBlogs.$keyAt(index), 'afBlogs keys: ', Object.keys($rootScope.afBlogs));

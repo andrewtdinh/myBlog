467011449306ee4c1f34577da9663510c8e67120
Andrew Dinh <andrewdinh74@yahoo.com>
2015-07-16 22:51:04 -0700

    Commit before trying another angular markdown directive

--- client/views/blog/fullBlog.js
+++ client/views/blog/fullBlog.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('ptc')
-.controller('FullBlogCtrl', ['Blog', '$scope', '$state', function(Blog, $scope, $state){
+.controller('FullBlogCtrl', ['Blog', '$scope', '$state', 'marked', function(Blog, $scope, $state, marked){
   $scope.name = $state.current.name;
   var blogKey = $state.params.blogKey;
   $scope.blog = Blog.getBlog(blogKey);

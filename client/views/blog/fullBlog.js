2b1c6ffda1a2d0c0b2b06e8806b8bfb91740bb3e
Andrew Dinh <andrewdinh74@yahoo.com>
2015-07-17 17:54:41 -0700

    Fix the injector error on the Full Blog page

--- client/views/blog/fullBlog.js
+++ client/views/blog/fullBlog.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('ptc')
-.controller('FullBlogCtrl', ['md', 'Blog', '$scope', '$state', function(md, Blog, $scope, $state){
+.controller('FullBlogCtrl', ['Blog', '$scope', '$state', function(Blog, $scope, $state){
   $scope.name = $state.current.name;
   var blogKey = $state.params.blogKey;
   $scope.blog = Blog.getBlog(blogKey);
